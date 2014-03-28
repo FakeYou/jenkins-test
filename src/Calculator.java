66e34d305714e58dd5b16f353aa2dd55b513a2db
FakeYou <ag.nanninga@gmail.com>
2014-03-28 11:36:10 +0100

    ruined everything

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
     }
 
     public static int subtract(int number1, int number2) {
-        return number1 - number2;
+        return number1 - number1;
     }
 
     public static int multiply(int number1, int number2) {
